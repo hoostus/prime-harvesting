97935677540ad2d3ebf70bb7c87a7efee998ce72
Justus Pendleton <justus@ego-pulse.com>
2016-06-14 17:16:29 +0700

    Move VPW to csv
    Move the VPW rates into a CSV file and add more series, to make it
    easier to compare VPW 30year vs 40year.

--- vpw.py
+++ vpw.py
@@ -1,55 +1,37 @@
 from decimal import Decimal
+import pandas
+import math
 
-# starting at age 65, 40-year span, based on 60% domestic stocks, 40% domestic bonds
-vpw_rates = [
-    Decimal('.047'),
-    Decimal('.047'),
-    Decimal('.048'),
-    Decimal('.049'),
-    Decimal('.049'),
-    Decimal('.050'),
-    Decimal('.051'),
-    Decimal('.051'),
-    Decimal('.052'),
-    Decimal('.053'),
-    Decimal('.054'),
-    Decimal('.055'),
-    Decimal('.056'),
-    Decimal('.057'),
-    Decimal('.059'),
-    Decimal('.060'),
-    Decimal('.062'),
-    Decimal('.063'),
-    Decimal('.065'),
-    Decimal('.067'),
-    Decimal('.069'),
-    Decimal('.072'),
-    Decimal('.075'),
-    Decimal('.078'),
-    Decimal('.081'),
-    Decimal('.085'),
-    Decimal('.090'),
-    Decimal('.095'),
-    Decimal('.101'),
-    Decimal('.109'),
-    Decimal('.117'),
-    Decimal('.128'),
-    Decimal('.142'),
-    Decimal('.159'),
-    Decimal('.182'),
-    Decimal('.215'),
-    Decimal('.264'),
-    Decimal('.346'),
-    Decimal('.509'),
-    Decimal('1.000'),
+_vpw = pandas.read_csv('vpw.csv')
+# Keep them all as strings, instead of converting to floats
+# Since we prefer Decimal...
+for i in range(1, 41):
+    _vpw[['Y%d' % i]] = _vpw[['Y%d' % i]].astype(str)
 
-    Decimal('0'),
-    Decimal('0'),
-    Decimal('0'),
-    Decimal('0'),
-    Decimal('0'),
-    Decimal('0'),
-    Decimal('0'),
-    Decimal('0'),
-    Decimal('0'),
-]
+def _lookup(type, year):
+    labels = [d[0] for d in _vpw.values]
+    i = labels.index(type)
+
+    row = _vpw.iloc[i]
+    assert(row[0] == type)
+    # row is 0-based but year is 1-based so we can just use it
+    # directly like this....
+    v = row[year]
+
+    # handle the NaNs from pandas. In practice we only hit them
+    # if we've exhausted the entire portfolio. So we should start
+    # withdrawing 0% after that....
+    if v == 'nan':
+        return Decimal(0)
+    else:
+        return Decimal(v)
+
+s_60_40_30 = [_lookup('60/40 30 years', n) for n in range(1, 41)]
+s_60_40_35 = [_lookup('60/40 35 years', n) for n in range(1, 41)]
+
+s_50_50_40 = [_lookup('50/50 40 years', n) for n in range(1, 41)]
+s_60_40_40 = [_lookup('60/40 40 years', n) for n in range(1, 41)]
+s_70_30_40 = [_lookup('70/30 40 years', n) for n in range(1, 41)]
+s_80_20_40 = [_lookup('80/20 40 years', n) for n in range(1, 41)]
+
+vpw_rates = s_60_40_40
