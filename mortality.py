ef5ed4afa010a1ba7ac99c353e41254480938e58
Justus Pendleton <justus@ego-pulse.com>
2016-07-24 11:30:15 +0700

    My mortality calculations were totally wrong
    Which is what accounted for a lot of the discrepancy between
    my WER results and Blanchett et al’s WER results. (I was making them
    die younger, which causes a bigger gap between the perfect information
    Sustainable Spending Rate.)
    
    Also, found some issues with the lifetable but that’s a future commit.

--- mortality.py
+++ mortality.py
@@ -1,40 +1,19 @@
-from decimal import Decimal
-import pandas
-import random
+import hultstrom_mortality as h
 
-_life = pandas.read_csv('2004-period-life-table.csv')
+def gen_age(age=65):
+    ''' Generates a final age for a couple (male + female) who are both
+    65 years old '''
 
-def _lookup(start_age, current_age, key):
-    r0 = _life.iloc[start_age][key]
-    r1 = _life.iloc[current_age][key]
+    return omy(True, True, age)
 
-    r0 = Decimal(r0.replace(',', ''))
-    r1 = Decimal(r1.replace(',', ''))
-    return (r0, r1)
+def omy(husband, wife, age):
+    h_omy = h.survive(age, h.MALE)
+    w_omy = h.survive(age, h.FEMALE)
 
-def male_mortality(start_age, current_age):
-    (r0, r1) = _lookup(start_age, current_age, 'Male Number of Lives')
-    return r1 / r0
+    husband = (husband and h_omy)
+    wife = (wife and w_omy)
 
-def female_mortality(start_age, current_age):
-    (r0, r1) = _lookup(start_age, current_age, 'Female Number of Lives')
-    return r1 / r0
-
-def both_alive(start_age, current_age):
-    return male_mortality(start_age, current_age) * female_mortality(start_age, current_age)
-def male_only_alive(start_age, current_age):
-    return male_mortality(start_age, current_age) * (1 - female_mortality(start_age, current_age))
-def female_only_alive(start_age, current_age):
-    return female_mortality(start_age, current_age) * (1 - male_mortality(start_age, current_age))
-def either_alive(start_age, current_age):
-    neither = (1 - male_mortality(start_age, current_age)) * (1 - female_mortality(start_age, current_age))
-    return 1 - neither
-
-def die(start_age, current_age):
-    if random.random() < either_alive(start_age, current_age):
-        return die(start_age, current_age + 1)
+    if husband or wife:
+        return omy(husband, wife, age + 1)
     else:
-        return current_age
-
-def gen_age():
-    return die(65, 65)
+        return age
