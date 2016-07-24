4680b821c2090eee73d4a7dd2ca146a4521fdcc2
Justus Pendleton <justus@ego-pulse.com>
2016-07-24 10:49:12 +0700

    CEW 0-value fixes
    CEW (and by extension WER) behaves poorly if there are any 0s
    in the cashflow list. A single zero means CEW always returns 0.
    That skews results a lot. To counter that, Blanchett et al add a tiny
    amount of cashflow every year.
    
    This adds an automatic fudge factor to WER and an assertion to
    CEW to check for 0s.

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 import operator
 import functools
 import numpy
+import builtins
 
 # numpy can't handle Decimal so we have this helper function
 def average(xs):
@@ -10,6 +11,10 @@ def mean(xs):
     return numpy.mean([float(x) for x in xs])
 def median(xs):
     return numpy.median([float(x) for x in xs])
+def min(xs):
+    return builtins.min([float(x) for x in xs])
+def max(xs):
+    return builtins.max([float(x) for x in xs])
 
 def prod(x):
     return functools.reduce(operator.mul, x, 1)
@@ -43,20 +48,29 @@ def hreff(withdrawals, returns):
 
     return cew_floor(withdrawals) / ssr(returns)
 
-def wer(withdrawals, returns):
+def wer(withdrawals, returns, fudge=Decimal('.001')):
     ''' Withdrawal Efficiency Rate
 
-        Given a list-liek of withdrawals and a list-like of actual returns over that same
+        Given a list-like of withdrawals and a list-like of actual returns over that same
         period, calculate how 'efficient' the withdrawals were.
+        
+        The withdrawals list is expected to be "percent of original portfolio size"
 
-        This is done by calculating the Sustainable
+        If there are any zeroes in the withdrawals, then the CEW calculation blows up which throws everything
+        off. So the 'fudge' is there to ensure there is at least a very small number.
 
         Described in Optimal Withdrawal Strategy for Retirement Income Portfolios (2012)
         by Blanchett, Kowara, Chen
         https://corporate.morningstar.com/us/documents/ResearchPapers/OptimalWithdrawalStrategyRetirementIncomePortfolios.pdf
+        
     '''
-
-    return cew(withdrawals) / ssr(returns)
+    c = cew([n + fudge for n in withdrawals])
+    s = ssr(returns) + fudge
+    if c > s:
+        from pprint import pprint
+        #import pdb;pdb.set_trace()
+    #assert c < s, "Found super-optimal solution, which is impossible. CEW = %s. SSR = %s." % (c, s)
+    return c / s
 
 
 def ssr(r):
@@ -74,8 +88,7 @@ def ssr(r):
             return (1 / denom) + ssr_seq(r[1:])
 
     # skip the final year of returns (since you have withdrawn the entire portfolio
-    # at the beginning of the year,
-    # knowing that you will die this year)
+    # at the beginning of the year, knowing that you will die this year)
     r_r = list(reversed(r[:-1]))
     return Decimal('1') / ssr_seq(r_r)
 
@@ -94,8 +107,10 @@ def cew(cashflows):
     however there are some who look at the asset allocation decisions people actually make that claim it must
     be 10 or more as a result."
     '''
-
-    # chosen somewhat arbitrarily by Blanchett et al point out that
+    
+    assert 0 not in cashflows, "Having a zero in the cashflows breaks CEW. 0 found at index %d" % cashflows.index(0)
+ 
+    # chosen somewhat arbitrarily by Blanchett et al who point out that
     # the final results aren't very sensitive to this number (i.e. changing it to
     # 2 doesn't affect the final numbers very much)
     gamma = Decimal('4.0')
