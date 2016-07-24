b98112a60fc288ff2477ed51c85ddc7b25589ab2
Justus Pendleton <justus@ego-pulse.com>
2016-07-24 10:46:20 +0700

    Handle empty portfolios

--- adt.py
+++ adt.py
@@ -9,12 +9,19 @@ YearlyResults = collections.namedtuple('YearlyResults',
      'portfolio_n', 'portfolio_r', 'portfolio_bonds', 'portfolio_stocks'])
 
 def report(portfolio, withdrawal, current_gains):
+    # If the portfolio has gone to 0 then we need to handle it slightly differently
+    # to avoid a division by zero error
+    if portfolio.value + withdrawal == 0:
+        withdraw_pct_cur = 0
+    else:
+        withdraw_pct_cur = withdrawal / (portfolio.value + withdrawal)
+   
     return YearlyResults(
         returns = current_gains,
+
         withdraw_n = withdrawal,
         withdraw_r = withdrawal / portfolio.inflation,
-
-        withdraw_pct_cur = withdrawal / (portfolio.value + withdrawal),
+        withdraw_pct_cur = withdraw_pct_cur,
         withdraw_pct_orig = (withdrawal / portfolio.inflation) / portfolio.starting_value,
 
         portfolio_n = portfolio.value,
