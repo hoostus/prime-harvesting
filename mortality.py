0a955a2e8f9aa8397580bb5bb26f020a20850a92
Justus Pendleton <justus@ego-pulse.com>
2016-07-24 10:53:59 +0700

    Change the mortality API
    Instead of assuming the start age is 65, make it an explicit parameter
    everywhere

--- mortality.py
+++ mortality.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 import pandas
+import random
 
 _life = pandas.read_csv('2004-period-life-table.csv')
 
@@ -11,34 +12,29 @@ def _lookup(start_age, current_age, key):
     r1 = Decimal(r1.replace(',', ''))
     return (r0, r1)
 
-def male_mortality(current_age, start_age=65):
+def male_mortality(start_age, current_age):
     (r0, r1) = _lookup(start_age, current_age, 'Male Number of Lives')
     return r1 / r0
 
-def female_mortality(current_age, start_age=65):
+def female_mortality(start_age, current_age):
     (r0, r1) = _lookup(start_age, current_age, 'Female Number of Lives')
     return r1 / r0
 
-def both_alive(m_age, f_age):
-    return male_mortality(m_age) * female_mortality(f_age)
-def male_only_alive(m_age, f_age):
-    return male_mortality(m_age) * (1 - female_mortality(f_age))
-def female_only_alive(m_age, f_age):
-    return female_mortality(f_age) * (1 - male_mortality(m_age))
-def either_alive(m_age, f_age):
-    neither = (1 - male_mortality(m_age)) * (1 - female_mortality(f_age))
+def both_alive(start_age, current_age):
+    return male_mortality(start_age, current_age) * female_mortality(start_age, current_age)
+def male_only_alive(start_age, current_age):
+    return male_mortality(start_age, current_age) * (1 - female_mortality(start_age, current_age))
+def female_only_alive(start_age, current_age):
+    return female_mortality(start_age, current_age) * (1 - male_mortality(start_age, current_age))
+def either_alive(start_age, current_age):
+    neither = (1 - male_mortality(start_age, current_age)) * (1 - female_mortality(start_age, current_age))
     return 1 - neither
 
-import random
-import numpy
-
-def die(i):
-    if random.random() < either_alive(i, i):
-        return die(i+1)
+def die(start_age, current_age):
+    if random.random() < either_alive(start_age, current_age):
+        return die(start_age, current_age + 1)
     else:
-        # they didn't live to i, so their final
-        # age was i-1
-        return i - 1
+        return current_age
 
 def gen_age():
-    return die(65)
+    return die(65, 65)
