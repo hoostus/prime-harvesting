192da0e2a68736c7d368ae0cd36c07b0e31df623
Justus Pendleton <justus@ego-pulse.com>
2016-08-11 15:45:29 +0700

    Add Pye's Retrenchment Rule
    Which is really just PMT with different parameters than VPW uses.

--- withdrawal.py
+++ withdrawal.py
@@ -3,6 +3,7 @@ from adt import report, AnnualChange
 from mufp import get_extended_mufp
 from vpw import vpw_rates
 import math
+from metrics import pmt
 
 # Things I don't like...
 # - Lots of repeated logic
@@ -161,6 +162,68 @@ class VPW(WithdrawalStrategy):
 
             index += 1
 
+class RetrenchmentRule(WithdrawalStrategy):
+    """ Gorden Pye's retrenchment rule is another PMT based solution. """
+    def __init__(self, portfolio, harvest_strategy, **kwargs):
+        super().__init__(portfolio, harvest_strategy, **kwargs)
+
+        self.discount_rate = .08
+        self.final_age = 110
+
+    def withdrawals(self):
+        # This assumes you are starting at age 65. There is no good way to
+        # use a different starting age here. Something like that really needs
+        # to be baked into the superclass. But that would require a bigger
+        # refactoring.....
+        current_age = 65
+        withdrawal = pmt(self.discount_rate, self.final_age - current_age, self.portfolio.value)
+        actual_withdrawal = self.harvest.send(withdrawal)
+        change = yield report(self.portfolio, actual_withdrawal, None)
+        assert isinstance(change, AnnualChange)
+
+        while True:
+            previous_portfolio_amount = self.portfolio.value
+            (gains, _, _) = self.portfolio.adjust_returns(change)
+
+            current_age += 1
+            if current_age < self.final_age:
+                withdrawal = pmt(self.discount_rate, self.final_age - current_age, self.portfolio.value)
+            else:
+                # We ran out of money and are still alive :(
+                withdrawal = 0
+            actual_withdrawal = self.harvest.send(withdrawal)
+            change = yield report(self.portfolio, actual_withdrawal, gains)
+            assert isinstance(change, AnnualChange)
+
+
+class ARVA(WithdrawalStrategy):
+    """ This comes from Siegel and Waring's 'Only Spending Rule You'll Ever Need' (2014)
+    https://larrysiegeldotorg.files.wordpress.com/2014/09/siegel_waring_only-spending-rule-article-youll-ever-need.pdf
+
+    Which is basically PMT, the same underlying rule as in VPW.
+
+    However, Siegel and Waring have different suggestions about how the discount_rate and "number of years"
+    should be calculated, so this has their defaults baked in.
+    """
+    def __init__(self, portfolio, harvest_strategy, discount_rate=Decimal('.02')):
+        # Siegel & Waring seem to suggest the discount rate should be
+        # "TIPS interest rate (present-value-weighted average interest rate across the TIPS
+        # ladder)" at the start of each year.
+        # Currently, we don't support changing the discount_rate every year and use a constant rate
+        super().__init__(portfolio, harvest_strategy)
+
+        self.discount_rate = discount_rate
+
+    # every year we need to
+    # Siegel & Waring suggest using the average of 120 (the maximum known human life span) and
+    # life expectancy based on current age according to the Social Security tables
+    # years_remaining = average(120, mean_life_expectancy)
+    # -numpy.pmt(discount_rate, years_remaining, portfolio.value, 0, 'start')
+
+    # Siegel & Waring's suggestion to continually update the assumptions make this
+    # tricky to model well
+
+
 # This one is super-simple...just take a constant percentage every
 # year. Don't try to index for inflation. So it will vary as the portfolio
 # value varies...
@@ -178,8 +241,7 @@ class ConstantPercentage(WithdrawalStrategy):
 
         while True:
             previous_portfolio_amount = self.portfolio.value
-            self.portfolio.adjust_returns(change)
-            gains = (self.portfolio.value - previous_portfolio_amount) / previous_portfolio_amount
+            (gains, _, _) = self.portfolio.adjust_returns(change)
 
             withdrawal = self.portfolio.value * self.rate
             actual_withdrawal = self.harvest.send(withdrawal)
