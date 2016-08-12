6a470ea310ddf37a0b33b4c7ec0fd8d68d0a1969
Justus Pendleton <justus@ryoohki.net>
2016-08-12 22:23:22 +0700

    I misunderstood how gummy's Sensible Withdrawals work. It is now fixed.

--- withdrawal/sensible.py
+++ withdrawal/sensible.py
@@ -4,32 +4,32 @@ from .abc import WithdrawalStrategy
 class SensibleWithdrawals(WithdrawalStrategy):
     """ Gummy's Sensible Withdrawals
 
-    The idea is: you get 80% of the inflation-adjusted amount you took
-    last year plus a percentage of any gains.
+    The 'sensible' idea is you set a floor and only get to withdraw
+    above that if the market was good last year.
 
-    You can pick any starting withdrawal rate and any percentage of gains.
+    gummy doesn't really tell you what to pick for the starting rate,
+    the floor, or how much of the profits you get to take.
 
-    McClung used 5% and 31% in his book, so I've mirrored that.
+    But he mention 5%, 3%, 25%, so we'll use those for default
+    parameters
 
     http://www.gummy-stuff.org/sensible_withdrawals.htm
     """
 
-    def __init__(self, portfolio, harvest_strategy, extra_return_boost=Decimal('.31'), initial_rate=Decimal('.05')):
+    def __init__(self, portfolio, harvest_strategy, extra_return_boost=Decimal('.25'), initial_rate=Decimal('.04'), min_rate=Decimal('.03')):
         super().__init__(portfolio, harvest_strategy)
 
-        self.initial_withdrawal = initial_rate * portfolio.value
+        self.floor = min_rate * portfolio.value
         self.extra_return_boost = extra_return_boost
         self.initial_rate = initial_rate
 
     def start(self):
-        withdrawal = self.initial_withdrawal
+        withdrawal = self.initial_rate * self.portfolio.value
         self.previous_portfolio_value = self.portfolio.value - withdrawal
-        self.previous_withdrawal = withdrawal
         return withdrawal
 
     def next(self):
-        inflation_adjusted_withdrawal_amount = self.previous_withdrawal * (1 + self.current_inflation)
-        current_withdrawal_amount = inflation_adjusted_withdrawal_amount * Decimal('.8')
+        current_withdrawal_amount = self.floor * (1 + self.current_inflation)
 
         base_portfolio_value = self.portfolio.value - current_withdrawal_amount
         adjusted_previous_portfolio = self.previous_portfolio_value * (1 + self.current_inflation)
@@ -39,8 +39,6 @@ class SensibleWithdrawals(WithdrawalStrategy):
             current_withdrawal_amount += extra_return * self.extra_return_boost
 
         current_withdrawal_amount = min(current_withdrawal_amount, self.portfolio.value)
-
-        self.previous_withdrawal = current_withdrawal_amount
         self.previous_portfolio_value = self.portfolio.value - current_withdrawal_amount
 
         return current_withdrawal_amount
