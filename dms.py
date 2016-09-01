62f86b614c9b03d0ec10945bdf70207fd10f4cd5
Justus Pendleton <justus@ego-pulse.com>
2016-09-01 11:34:37 +0700

    DMS returns data for monte carlo inputs
    These are intended for use in Monte Carlo analyses

---
+++ dms.py
@@ -0,0 +1,114 @@
+# From Credit Suisse Global Investment Returns Yearbook 2012
+# 2012 was the last year they published standard deviation :(
+# All numbers are 1900-2012
+# Tuple is (real_returns, nominal_returns, standard_deviation)
+australia = {
+    'equities' : (7.2, 11.3, 18.2),
+    'bonds' : (1.6, 5.5, 13.2),
+    'bills' : (0.7, 4.6, 5.4)
+}
+belgium = {
+    'equities' : (2.4, 7.8, 23.6),
+    'bonds' : (-0.1, 5.2, 11.9),
+    'bills' : (-0.4, 4.9, 8.0)
+}
+canada = {
+    'equities' : (5.7, 8.9, 17.2),
+    'bonds' : (2.2, 5.3, 10.4),
+    'bills' : (1.6, 4.6, 4.9)
+}
+denmark = {
+    'equities' : (4.9, 8.9, 20.9),
+    'bonds' : (3.2, 7.2, 11.7),
+    'bills' : (2.2, 6.2, 6.0)
+}
+finland = {
+    'equities' : (5.0, 12.6, 30.4),
+    'bonds' : (-0.2, 7.1, 13.6),
+    'bills' : (-0.5, 6.7, 11.8)
+}
+france = {
+    'equities' : (2.9, 10.2, 23.5),
+    'bonds' : (-0.1, 7.1, 13.0),
+    'bills' : (-2.8, 4.1, 9.5)
+}
+germany = {
+    'equities' : (2.9, 8.1, 32.2),
+    'bonds' : (-1.8, 2.9, 15.6),
+    'bills' : (-2.4, 2.2, 13.3)
+}
+ireland = {
+    'equities' : (3.7, 8.1, 23.1),
+    'bonds' : (0.9, 5.2, 14.8),
+    'bills' : (0.7, 4.9, 6.6)
+}
+italy = {
+    'equities' : (1.7, 10.2, 29.0),
+    'bonds' : (-1.7, 6.5, 14.0),
+    'bills' : (-3.6, 4.5, 11.5)
+}
+japan = {
+    'equities' : (3.6, 10.8, 29.8),
+    'bonds' : (-1.1, 5.8, 20.0),
+    'bills' : (-1.9, 4.9, 13.9)
+}
+netherlands = {
+    'equities' : (4.8, 7.9, 21.8),
+    'bonds' : (1.5, 4.5, 9.4),
+    'bills' : (0.7, 3.6, 4.9)
+}
+new_zealand = {
+    'equities' : (5.8, 9.7, 19.7),
+    'bonds' : (2.1, 5.9, 9.1),
+    'bills' : (1.7, 5.5, 4.7)
+}
+norway = {
+    'equities' : (4.1, 7.9, 27.3),
+    'bonds' : (1.8, 5.6, 12.2),
+    'bills' : (1.2, 4.9, 7.1)
+}
+south_africa = {
+    'equities' : (7.2, 12.5, 22.5),
+    'bonds' : (1.8, 6.8, 10.3),
+    'bills' : (1.0, 6.0, 6.2)
+}
+spain = {
+    'equities' : (3.4, 9.4, 22.2),
+    'bonds' : (1.3, 7.2, 11.7),
+    'bills' : (0.3, 6.1, 5.8)
+}
+sweden = {
+    'equities' : (6.1, 9.9, 22.9),
+    'bonds' : (2.6, 6.2, 12.4),
+    'bills' : (1.8, 5.5, 6.8)
+}
+switzerland = {
+    'equities' : (4.1, 6.5, 19.7),
+    'bonds' : (2.2, 4.5, 9.3),
+    'bills' : (0.8, 3.1, 5.0)
+}
+uk = {
+    'equities' : (5.2, 9.4, 19.9),
+    'bonds' : (1.5, 5.5, 13.8),
+    'bills' : (1.0, 5.0, 6.4)
+}
+us = {
+    'equities' : (6.2, 9.3, 20.2),
+    'bonds' : (2.0, 5.0, 10.3),
+    'bills' : (0.9, 3.9, 4.7)
+}
+world = {
+    'equities' : (5.4, 8.5, 17.7)
+    'bonds' : (1.7, 4.8, 10.4),
+    'bills' : (0.9, 3.9, 4.7)
+}
+ex_us {
+    'equities' : (4.8, 8.0, 20.4),
+    'bonds' : (1.3, 4.3, 14.2),
+    'bills' : (0.9, 3.9, 4.7)
+}
+europe = {
+    'equities' : (4.6, 7.7, 21.5),
+    'bonds' : (0.9, 3.9, 15.3),
+    'bills' : (0.9, 3.9, 4.7)
+}
