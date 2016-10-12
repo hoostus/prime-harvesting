41454e9fb6126be11302061b5d45f197d980399a
Justus Pendleton <justus@ego-pulse.com>
2016-10-12 11:02:58 +0700

    Make pylint happy

--- perfect.py
+++ perfect.py
@@ -1,38 +1,48 @@
-# This is based on Suarez et al's "The Perfect Withdrawal Amount" (2014)
+"""This is based on Suarez et al's "The Perfect Withdrawal Amount" (2014)"""
+
+from decimal import Decimal
+
+import numpy
+import pandas
 
 import metrics
-from decimal import Decimal
-import pandas
-import numpy
-import mortality
+
 
 def create_distribution(engine_factory, start, end, year_factory=lambda: 30, iterations=20000):
+    """ This creates a distribution of maximum withdrawal rates based on the inputs. """
     series = pandas.Series(index=numpy.arange(0, iterations))
     def weight(annual):
+        """ We need to convert the AnnualChange to a single number. Assume 60/40 weighting. """
         return (annual.stocks * Decimal('.6')) + (annual.bonds * Decimal('.4'))
 
     for i in range(iterations):
-        e = engine_factory()
-        r = [weight(e.random_year()) for _ in range(year_factory())]
-        series.iloc[i] = float(metrics.pwa(start, end, r))
+        engine = engine_factory()
+        returns = [weight(engine.random_year()) for _ in range(year_factory())]
+        series.iloc[i] = float(metrics.pwa(start, end, returns))
     return series
 
 if __name__ == '__main__':
-    import montecarlo
-    import scipy.stats
+    def run_main():
+        """ For command line testing. """
+        import montecarlo
+        import mortality
+        import scipy.stats
 
-    balance = 2473053
-    series = create_distribution(lambda: montecarlo.conservative[50],
-        balance, 0,
-        iterations=20000, year_factory=lambda: 45)
-    #print(series.head())
+        # Using mortality makes it take A LOT longer
+        year_factory = lambda: mortality.gen_lifespan(mortality.DEFAULT_COUPLE)
+        year_factory = lambda: 45
 
-    # Using mortality makes it take A LOT longer
-    #year_factory=lambda: mortality.gen_lifespan(mortality.DEFAULT_COUPLE))
+        balance = 2473053
+        series = create_distribution(lambda: montecarlo.conservative[50],
+                                     balance, 0, iterations=20000,
+                                     year_factory=year_factory)
+        #print(series.head())
 
-    print('10th=', "${:,}".format(int(series.quantile(.1))))
-    print('50th=', "${:,}".format(int(series.quantile(.5))))
+        print('10th=', "${:,}".format(int(series.quantile(.1))))
+        print('50th=', "${:,}".format(int(series.quantile(.5))))
 
-    # For a given income what percentile is it in the distribution?
-    income = 50000
-    print("${:,}= ".format(income), scipy.stats.percentileofscore(series, income))
+        # For a given income what percentile is it in the distribution?
+        income = 50000
+        print("${:,}= ".format(income), scipy.stats.percentileofscore(series, income))
+
+    run_main()
