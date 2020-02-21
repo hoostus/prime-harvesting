d9d3d7a6101ad7c23bcf38a45d2ccfbc585e6681
Justus Pendleton <justus@ryoohki.net>
2020-02-21 11:46:39 +0700

    fix bugs...annual working, monthly isn't
    Need to come up with a decent way to handle monthly...

--- bond_simulator.py
+++ bond_simulator.py
@@ -20,7 +20,7 @@ def a2m(annual_rate):
     return pow(annual_rate + 1, 1/12) - 1
 
 class Bond:
-    def __init__(self, face_value, yield_pct, maturity, payments_per_year=12):
+    def __init__(self, face_value, yield_pct, maturity, payments_per_year=1):
         self.face_value = face_value
         self.yield_pct = yield_pct
         self.maturity = maturity
@@ -33,7 +33,7 @@ class Bond:
         return self.face_value * self.yield_pct / self.payments_per_year
     
     def value(self, rates):
-        value = numpy.pv(rates[self.maturity - 1], self.maturity / 12, (self.face_value * self.yield_pct), self.face_value)
+        value = numpy.pv(rates[self.maturity - 1], self.maturity, (self.face_value * self.yield_pct), self.face_value)
         return -value
     
 class BondLadder:
@@ -77,23 +77,19 @@ class BondLadder:
             bond.maturity -= 1
 
     def sell_bonds(self, rates):
-        to_sell = filter(lambda bond: bond.maturity <= self.min_maturity, self.ladder)
+        to_sell = filter(lambda bond: bond.maturity < self.min_maturity, self.ladder)
         to_sell = list(to_sell)
         self.ladder = self.ladder.difference(to_sell)
         self.cash += sum((b.value(rates) for b in to_sell))
         return to_sell
 
-def bootstrap(yield_curve, max_bonds, min_maturity):
-    bond_yield = yield_curve[max_bonds - 1]
-
-    # Why - 11?
-    #min_maturity -= 11
-
+def bootstrap(yield_curve, max_bonds, min_maturity, monthly=False):
     ladder = BondLadder(min_maturity, max_bonds)
     starting_face_value = 50 # chosen arbitrarily (to match longinvest)
 
-    for i, j in zip(range(max_bonds), range(min_maturity, max_bonds+1)):
-        face_value = pow(1 + a2m(bond_yield), i) * starting_face_value
+    for j in range(min_maturity, max_bonds+1):
+        bond_yield = yield_curve[max_bonds-1]
+        face_value = pow(1 + bond_yield, j-min_maturity) * starting_face_value
         b = Bond(face_value, bond_yield, j)
         ladder.add_bond(b)
     return ladder
@@ -105,26 +101,17 @@ def loop(ladder, rates, max_maturity):
     # But that's okay because we overwrite them with later data
     # (since they all have the same year)
     for (year, current_rates) in rates:
-        if year.year % 5 == 0 and year.month == 1:
-            print('Calculating...', year.year)
-        (ladder, nav) = iterate_fund(ladder, current_rates, max_maturity)
+#        if year.year % 5 == 0 and year.month == 1:
+#            print('Calculating...', year.year)
+        (ladder, nav) = iterate_fund(ladder, current_rates.tolist(), max_maturity)
         df.loc[year] = {'NAV' : nav, 'Change' : None}
 
     calculate_returns(df)
     return df
 
 def calculate_returns(df):
-    # Longinvest calculates the return based on comparison's to
-    # next year's NAV. So I'll do the same. Even though that seems
-    # weird to me. Maybe it's because the rates are based on January?
-    # Hmmm...that sounds plausible.
-    max_row = df.shape[0]
-
-    for i in range(max_row - 1):
-        next_nav = df.iloc[i+1]['NAV']
-        nav = df.iloc[i]['NAV']
-        change = (next_nav - nav) / nav
-        df.iloc[i]['Change'] = change
+    change = df / df.shift(1) - 1
+    df['Change'] = change.shift(-1)
     return df
 
 def make_annual_ladder(max_maturity, min_maturity, yields):
@@ -143,10 +130,25 @@ def make_annual_ladder(max_maturity, min_maturity, yields):
     return ladder
 
 def simulate_monthly_turnover(max_maturity, min_maturity, rates):
+    """
+    rates is expected to be a pandas dataframe with one column per month
+    up to max_maturity * 12 columns (i.e. 10 years requires 120 monthly columns)
+    """
     min_maturity = min_maturity * 12
     max_maturity = max_maturity * 12
 
     initial_yields = rates.iloc[0]
-    ladder = bootstrap(initial_yields, max_maturity, min_maturity)
-
+    ladder = bootstrap(initial_yields, max_maturity, min_maturity, monthly=True)
     return loop(ladder, rates.iterrows(), max_maturity)
+
+def simulate_annual_turnover(max_maturity, min_maturity, rates):
+    """
+    rate is expected to be a pandas dataframe with one column per year
+    up to max maturity columns (10 years == 10 columns)
+    """
+    initial_yields = rates.iloc[0].tolist()
+    yields = rates.iterrows()
+    #initial_yields = rates['1970'].values.tolist()[0]
+    #yields = rates['1970':].iterrows()
+    ladder = bootstrap(initial_yields, max_maturity, min_maturity)
+    return loop(ladder, yields, max_maturity)
